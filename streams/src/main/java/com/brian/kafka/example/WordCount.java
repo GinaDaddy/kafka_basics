d5126d36157a29f5e804895c7fc0bfd6c27e033d
Brian Moon <brian.dhmoon@gmail.com>
2020-02-04 20:08:43 +1100

    Add apache kafka stream examples using simple transformation by flatMap, use of groupBy and materialized store

---
+++ streams/src/main/java/com/brian/kafka/example/WordCount.java
@@ -0,0 +1,70 @@
+package com.brian.kafka.example;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.state.KeyValueStore;
+
+/**
+ * http://kafka.apache.org/24/documentation/streams/tutorial
+ * Example of using groupBy and Materialized store
+ */
+public class WordCount {
+    public static void main(String[] args) {
+        // Streams execution configuration
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-wordcount");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        // Define computational logic of our stream or topology
+        final StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> source = builder.stream("streams-plaintext-input");
+        source.flatMapValues(value -> Arrays.asList(value.toLowerCase(Locale.getDefault()).split("\\W+")))
+            // counting aggregation
+            .groupBy((key, value) -> value)
+            // Materialize the result into a KeyValueStore named "counts-store".
+            // The Materialized store is always of type <Bytes, byte[]> as this is the format of the inner most store
+            .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("counts-store"))
+            .toStream()
+            .to("streams-wordcount-output", Produced.with(Serdes.String(), Serdes.Long()));
+
+        final Topology topology = builder.build();
+
+        System.out.println(topology.describe());
+
+        // If you're happy with topology printed above, then move on to construct the streams
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);
+    }
+}

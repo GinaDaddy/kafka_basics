3d4e26897e4b3615189ab187c50dd2fe1ac16f02
Brian Moon <bmoon@homeaway.com>
2020-03-21 16:20:59 +1100

    Demo serialize/deserialize users

---
+++ src/main/java/com/brian/avro/UserSerializationWithCodeGeneration.java
@@ -0,0 +1,63 @@
+package com.brian.avro;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import com.brian.avro.model.User;
+
+public class UserSerializationWithCodeGeneration {
+
+    public void testUserSerialization(File file) throws IOException {
+        User user1 = new User();
+        user1.setName("Alyssa");
+        user1.setFavoriteNumber(256);
+        // Leave favorite color null
+
+        // Alternate constructor
+        User user2 = new User("Ben", 7, "red");
+
+        // Construct via builder
+        User user3 = User.newBuilder()
+                        .setName("Charlie")
+                        .setFavoriteColor("blue")
+                        .setFavoriteNumber(null)
+                        .build();
+
+        // Serialize user1, user2 and user3 to disk
+        DatumWriter<User> userDatumWriter = new SpecificDatumWriter<>(User.class);
+        DataFileWriter<User> dataFileWriter = new DataFileWriter<>(userDatumWriter);
+        dataFileWriter.create(user1.getSchema(), file);
+        dataFileWriter.append(user1);
+        dataFileWriter.append(user2);
+        dataFileWriter.append(user3);
+        dataFileWriter.close();
+    }
+
+    public void testUserDeserialization(File file) throws IOException {
+        // Deserialize Users from disk
+        DatumReader<User> userDatumReader = new SpecificDatumReader<>(User.class);
+        DataFileReader<User> dataFileReader = new DataFileReader<User>(file, userDatumReader);
+        User user = null;
+        while (dataFileReader.hasNext()) {
+            user = dataFileReader.next(user);
+            System.out.println(user);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        File file = new File("users.avro");
+
+        UserSerializationWithCodeGeneration demo = new UserSerializationWithCodeGeneration();
+        demo.testUserSerialization(file);
+        Thread.sleep(1000);
+        demo.testUserDeserialization(file);
+
+    }
+}
